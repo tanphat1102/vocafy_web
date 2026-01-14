d331dd501b135f4824eb1779a5b459055355a86a
tanphat1102 <chopml1102@gmail.com>
2026-01-14 13:07:02 +0700

    Move Firebase config to environment variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAC_S4iycnGdKo2YvU5onuq8zorpzuAIfA",
-  authDomain: "vocafy-b13e3.firebaseapp.com",
-  projectId: "vocafy-b13e3",
-  storageBucket: "vocafy-b13e3.firebasestorage.app",
-  messagingSenderId: "249112075196",
-  appId: "1:249112075196:web:1be54bd82a9599362f69b9",
-  measurementId: "G-6P84Q0WYC8",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
